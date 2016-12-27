b5083278d82102131d3d442221f64ad09102bbe4
Srie Raam Mohan <srieraam@meise209.wohnheim.uni-kl.de>
2016-12-27 02:07:26 +0100

    Added two more visualizations

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ Visualization of the battle data in different regions
 
 Legend:
 [0: 'Beyond the Wall', 1: 'The Crownlands', 2: 'The North', 3: 'The Reach', 4: 'The Riverlands', 5: 'The Stormlands', 6: 'The Westerlands']
+
+### Visualization 2 :
+Visualization of character deaths gender-wise
+![battle](output/deaths_visualization.png)
+
+Legend:
+[0: 'Female', 1: 'Male']
+We can see that male and female who are alive are generally grouped together and those allegiances are relatively same.
+
+### Visualization 1 :
+Visualization of the prediction and popularity data gender-wise
+![battle](output/prediction_visualization.png)
+
+Legend:
+[0: 'Female', 1: 'Male']
+This dataset is not so different from the above dataset except with additional information. Mostly the visualization remains same with some modulation relative to the popularity data.
--- tsne_predictions.py
+++ tsne_predictions.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
 # Step 2: Load the dataset
-battles = pd.read_csv("dataset/battles.csv")
+battles = pd.read_csv("dataset/character-predictions.csv")
 
 # Step 3: Clean the seperate the dataset
-label_data = battles["region"]
-battles_data = battles.drop(battles.columns[[0, 1, 2, 6, 7, 8, 10, 11, 12, 19, 20, 23, 24]], axis=1).fillna("Nan")
+label_data = battles["male"]
+deaths_data = battles.drop(battles.columns[[0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 21, 22, 23, 26, 32]],axis=1).fillna("Nan")
 
 # Step 5: Encode feature data
 le = LabelEncoder()
-for col in battles_data.columns.values:
-    if battles_data[col].dtypes == "object":
-        unique_labels = np.unique(battles_data[col])
+for col in deaths_data.columns.values:
+    if deaths_data[col].dtypes == "object":
+        unique_labels = np.unique(deaths_data[col])
         le.fit(unique_labels)
-        battles_data[col] = le.transform(battles_data[col])
+        deaths_data[col] = le.transform(deaths_data[col])
 
 # Step 6: Encode label data
 le.fit(np.unique(label_data))
@@ -26,14 +26,15 @@ label_data = le.transform(label_data)
 
 # t-distributed Stochastic Neighbor Embedding (t-SNE) visualization
 tsne = TSNE(n_components=2, random_state=0)
-x_test_2d = tsne.fit_transform(battles_data)
+x_test_2d = tsne.fit_transform(deaths_data)
 
 # scatter plot the sample points among 5 classes
-markers = ('s', 'd', 'o', '^', 'v', '+', '*')
-color_map = {0: 'red', 1: 'blue', 2: 'lightgreen', 3: 'purple', 4: 'cyan', 5: 'magenta', 6: 'yellow'}
+markers = ('s', 'd')
+color_map = {0: 'red', 1: 'blue'}
 plt.figure()
 for idx, cl in enumerate(np.unique(label_data)):
-    plt.scatter(x=x_test_2d[label_data == cl, 0], y=x_test_2d[label_data == cl, 1], c=color_map[idx], marker=markers[idx], label=cl)
+    plt.scatter(x=x_test_2d[label_data == cl, 0], y=x_test_2d[label_data == cl, 1], c=color_map[idx],
+                marker=markers[idx], label=cl)
 plt.xlabel('X in t-SNE')
 plt.ylabel('Y in t-SNE')
 plt.legend(loc='upper left')
